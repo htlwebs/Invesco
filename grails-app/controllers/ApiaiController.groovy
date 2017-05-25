3e2fe165bf4b339488a3e9a7a980dd905be70e02
Haresh Luhana <haresh.luhana@mapmymarketing.ai>
2017-05-25 16:10:13 +0530

    Changes added q and answer

--- grails-app/controllers/ApiaiController.groovy
+++ grails-app/controllers/ApiaiController.groovy
@@ -1,12 +1,18 @@
 import grails.converters.JSON
+import invesco.DataPushService
+import invesco.InvescoAiEntry
 import ai.api.model.AIResponse
-class ApiAiController {
+class ApiaiController {
 	String urls="http://35.154.92.3:8080/Invesco/"
 	//String urls="http://192.168.0.222:8080/Invesco/"
 	def index() { }
+	def qq()
+	{
+		DataPushService oo= new DataPushService()
+	println	oo.test()
+	}
 	String action=""
-	def getAidata() {
-
+	def getAidata() {	
 		String username;
 		def jsonData=[]
 		def suggestion=[]
@@ -14,7 +20,18 @@ class ApiAiController {
 		String period=""
 		AIResponse response=TextClientApplication.getData(params.inputData)
 		if(response!=null) {
-
+			try{
+				InvescoAiEntry newQ= new InvescoAiEntry()
+				newQ.empcode=params.empcode
+				newQ.que=params.inputData
+				newQ.ans=response.getResult().getFulfillment().getSpeech() as String
+				newQ.time= new java.sql.Timestamp(new Date().getTime())
+				newQ.save()
+					}
+				catch(Exception e)
+				{
+					println"Error in storing answer....${e}"
+				}
 			if(response.getStatus().getCode()==200) {
 				println "Speech:"+response.getResult().getFulfillment().getSpeech()
 				println "Action:"+ response.getResult().getAction()
